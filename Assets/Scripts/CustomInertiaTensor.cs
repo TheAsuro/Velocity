97702cba50c967bab181d4c46203562b7abb0474
TheAsuro <theasuro@gmail.com>
2014-07-08 03:32:05 +0200

    Beacon marking the end of a level

---
+++ Assets/Scripts/CustomInertiaTensor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomInertiaTensor : MonoBehaviour
+{
+	public Vector3 inertiaTensor = Vector3.one;
+
+	void Start()
+	{
+		rigidbody.inertiaTensor = inertiaTensor;
+	}
+}
---
+++ Assets/Scripts/Turn.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turn : MonoBehaviour
+{
+	public float degrees = 360;
+	public float time = 1f;
+	public axis rotationAxis = axis.Y;
+
+	public enum axis
+	{
+		X,
+		Y,
+		Z
+	}
+
+	void Update()
+	{
+		float rotationAmount = (Time.time / time) * degrees;
+		Vector3 rotationVector;
+
+		if(rotationAxis == axis.X)
+		{
+			rotationVector = new Vector3(rotationAmount, transform.rotation.y, transform.rotation.z);
+		}
+		else if(rotationAxis == axis.Y)
+		{
+			rotationVector = new Vector3(transform.rotation.x, rotationAmount, transform.rotation.z);
+		}
+		else
+		{
+			rotationVector = new Vector3(transform.rotation.x, transform.rotation.y, rotationAmount);
+		}
+
+		transform.rotation = Quaternion.Euler(rotationVector);
+	}
+}
