cfa546dc7540ca7cabfd468a207b2c62f4eafdeb
TheAsuro <theasuro@gmail.com>
2014-07-21 20:39:11 +0200

    UI stuff

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -11,6 +11,7 @@ public class MainMenu : MonoBehaviour
 	private bool drawSelectMap = false;
 	private string nameFieldText = "name";
 	private int selectedNewGameIndex = -1;
+	private GUISkin skin;
 
 	private List<string> saveNames = new List<string>();
 
@@ -26,6 +27,7 @@ public class MainMenu : MonoBehaviour
 	void Start()
 	{
 		GameInfo.info.setMenuLocked(true);
+		skin = GameInfo.info.skin;
 	}
 
 	private void setState(State state)
@@ -84,61 +86,61 @@ public class MainMenu : MonoBehaviour
 		//Main buttons
 		if(drawMainButtons)
 		{
-			GUI.Box(centerMenuPos, "");
+			GUI.Box(centerMenuPos, "", skin.box);
 			GUILayout.BeginArea(centerMenuPos);
-			if(GUILayout.Button("New")) { setState(State.newGame); }
-			if(GUILayout.Button("Load")) { setState(State.loadGame); }
-			if(GUILayout.Button("Quit")) { Application.Quit(); }
+			if(GUILayout.Button("New", skin.button)) { setState(State.newGame); }
+			if(GUILayout.Button("Load", skin.button)) { setState(State.loadGame); }
+			if(GUILayout.Button("Quit", skin.button)) { Application.Quit(); }
 			GUILayout.EndArea();
 		}
 
 		//New game
 		if(drawSelectNewGame)
 		{
-			GUI.Box(centerMenuPos, "");
+			GUI.Box(centerMenuPos, "", skin.box);
 			GUILayout.BeginArea(centerMenuPos);
 			GUILayout.Label("New");
-			if(GUILayout.Button("1: " + saveNames[0])) { selectedNewGameIndex = 1; setState(State.enterName); }
-			if(GUILayout.Button("2: " + saveNames[1])) { selectedNewGameIndex = 2; setState(State.enterName); }
-			if(GUILayout.Button("3: " + saveNames[2])) { selectedNewGameIndex = 3; setState(State.enterName); }
-			if(GUILayout.Button("Back")) { setState(State.main); }
+			if(GUILayout.Button("1: " + saveNames[0], skin.button)) { selectedNewGameIndex = 1; setState(State.enterName); }
+			if(GUILayout.Button("2: " + saveNames[1], skin.button)) { selectedNewGameIndex = 2; setState(State.enterName); }
+			if(GUILayout.Button("3: " + saveNames[2], skin.button)) { selectedNewGameIndex = 3; setState(State.enterName); }
+			if(GUILayout.Button("Back", skin.button)) { setState(State.main); }
 			GUILayout.EndArea();
 		}
 
 		//Load game
 		if(drawSelectLoadGame)
 		{
-			GUI.Box(centerMenuPos, "");
+			GUI.Box(centerMenuPos, "", skin.box);
 			GUILayout.BeginArea(centerMenuPos);
 			GUILayout.Label("Load");
-			if(GUILayout.Button("1: " + saveNames[0])) { loadGame(1); }
-			if(GUILayout.Button("2: " + saveNames[1])) { loadGame(2); }
-			if(GUILayout.Button("3: " + saveNames[2])) { loadGame(3); }
-			if(GUILayout.Button("Back")) { setState(State.main); }
+			if(GUILayout.Button("1: " + saveNames[0], skin.button)) { loadGame(1); }
+			if(GUILayout.Button("2: " + saveNames[1], skin.button)) { loadGame(2); }
+			if(GUILayout.Button("3: " + saveNames[2], skin.button)) { loadGame(3); }
+			if(GUILayout.Button("Back", skin.button)) { setState(State.main); }
 			GUILayout.EndArea();
 		}
 
 		//select map
 		if(drawSelectMap)
 		{
-			GUI.Box(centerMenuPos, "");
+			GUI.Box(centerMenuPos, "", skin.box);
 			GUILayout.BeginArea(centerMenuPos);
 			GUILayout.Label("Map");
-			if(GUILayout.Button("Basic Tutorial")) { loadMap("BasicTutorial"); }
-			if(GUILayout.Button("Air Strafe Tutorial")) { loadMap("StrafeTutorial"); }
-			if(GUILayout.Button("Map 1")) { loadMap("Level3"); }
+			if(GUILayout.Button("Basic Tutorial", skin.button)) { loadMap("BasicTutorial"); }
+			if(GUILayout.Button("Air Strafe Tutorial", skin.button)) { loadMap("StrafeTutorial"); }
+			if(GUILayout.Button("Map 1", skin.button)) { loadMap("Level3"); }
 			GUILayout.EndArea();
 		}
 
 		//Enter name for new game
 		if(drawNameField)
 		{
-			GUI.Box(centerMenuPos, "");
+			GUI.Box(centerMenuPos, "", skin.box);
 			GUILayout.BeginArea(centerMenuPos);
 			GUILayout.Label("Enter name");
 			nameFieldText = GUILayout.TextField(nameFieldText);
-			if(GUILayout.Button("Back")) { setState(State.newGame); }
-			if(GUILayout.Button("OK"))
+			if(GUILayout.Button("Back", skin.button)) { setState(State.newGame); }
+			if(GUILayout.Button("OK", skin.button))
 			{
 				newGame(selectedNewGameIndex);
 			}
