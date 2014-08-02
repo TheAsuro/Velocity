da179ddb3c1ccbc0a2eec83b4032fb412d717671
TheAsuro <theasuro@gmail.com>
2014-08-02 03:47:38 +0200

    Added a console

---
+++ Assets/Scripts/Console.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Console : MonoBehaviour
+{
+	public bool drawConsole = false;
+	public Rect consolePos;
+
+	private string consoleText = "Welcome to Velocity!";
+	private string commandLine = "";
+
+	void Update()
+	{
+		if(Input.GetButtonDown("Console"))
+		{
+			drawConsole = !drawConsole;
+		}
+	}
+
+	void OnGUI()
+	{
+		if(drawConsole)
+		{
+			consolePos = GUILayout.Window(0, consolePos, DrawConsoleGUI, "Console", GameInfo.info.skin.window);
+		}
+	}
+
+	void DrawConsoleGUI(int id)
+	{
+		GUILayout.TextArea(consoleText, GUILayout.ExpandHeight(true));
+		GUI.SetNextControlName("cmd");
+		commandLine = GUILayout.TextField(commandLine);
+
+		UnityEngine.Event e = UnityEngine.Event.current;
+        if(e.keyCode == KeyCode.Return && GUI.GetNameOfFocusedControl().Equals("cmd") && !commandLine.Equals(""))
+        {
+        	writeToConsole(">" + commandLine);
+			executeCommand(commandLine);
+			commandLine = "";
+        }
+
+		GUI.DragWindow();
+	}
+
+	private void executeCommand(string command)
+	{
+		switch(command.ToLower())
+		{
+			case "quit":
+				Application.Quit();
+				break;
+			default:
+				writeToConsole("'" + command + "' is not a valid command!");
+				break;
+		}
+	}
+
+	public void writeToConsole(string content)
+	{
+		consoleText += "\n" + content; 
+	}
+}
--- Assets/Scripts/Special Levels/StrafeTutorial.cs
+++ Assets/Scripts/Special Levels/StrafeTutorial.cs
@@ -4,12 +4,10 @@ using System.Collections.Generic;
 
 public class StrafeTutorial : MonoBehaviour
 {
-	private Movement movementScript;
 	private string state = "start";
 
 	void Start()
 	{
-		movementScript = GameInfo.info.getPlayerObject().GetComponent<Movement>();
 		nextState();
 	}
 
@@ -17,46 +15,7 @@ public class StrafeTutorial : MonoBehaviour
 	{
 		switch(state)
 		{
-			case "start":
-				playWatchDemo();
-				state = "watch";
-				break;
-			case "watch":
-				playSimpleControls();
-				state = "simple";
-				break;
-			case "simple":
-				playAdvancedControls();
-				state = "advanced";
-				break;
-			case "advanced":
-				playFullControls();
-				state = "full";
-				break;
+			
 		}
-	}
-
-	private void playWatchDemo()
-	{
-		//Watch only
-		movementScript.setPlayerControls(false,false,false,false,false,false,false);
-	}
-
-	private void playSimpleControls()
-	{
-		//Move left and right with automatic air-strafing
-		movementScript.setPlayerControls(false,false,false,false,true,false,false);
-	}
-
-	private void playAdvancedControls()
-	{
-		//Move left and right with manual air-strafing
-		movementScript.setPlayerControls(false,false,false,false,true,false,true);
-	}
-
-	private void playFullControls()
-	{
-		//Full control
-		movementScript.setPlayerControls(true,true,true,true,true,true,true);
 	}
 }
