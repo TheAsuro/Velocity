bb22e6b09ffba896d084452d62046de1d90f6ae6
Till W. <theasuro@gmail.com>
2015-02-27 01:25:30 +0100

    Reverted to old ReplaceUiText script

--- Assets/Scripts/UI/ReplaceUiText.cs
+++ Assets/Scripts/UI/ReplaceUiText.cs
@@ -4,38 +4,89 @@ using System.Collections;
 
 public class ReplaceUiText : MonoBehaviour
 {
-    private string originalText;
-    private string text
+    private Text textScript
     {
-        get { return GetComponent<Text>().text; }
-        set { GetComponent<Text>().text = value; }
+        get { return GetComponent<Text>(); }
     }
 
-    void Awake()
+    private string initialText = "";
+    private SaveData player1, player2, player3;
+    private string wr = "";
+    private bool loadingWr = false;
+    private string pb = "";
+
+    void Start()
     {
-        originalText = text;
+        initialText = textScript.text;
+
+        init();
+    }
+
+    public void init()
+    {
+        player1 = new SaveData(1);
+        player2 = new SaveData(2);
+        player3 = new SaveData(3);
     }
 
     void Update()
     {
-        string tempText = originalText;
+        string temp = initialText;
+        SaveData playerSave = GameInfo.info.getCurrentSave();
 
-        //$currentplayer
-        SaveData sd = GameInfo.info.getCurrentSave();
-        if (sd == null)
-            tempText.Replace("$currentplayer", "No player selected!");
-        else
-            tempText.Replace("$currentplayer", sd.getPlayerName());
+        if (temp.Contains("$player1")) { temp = temp.Replace("$player1", player1.getPlayerName()); }
+        else if (temp.Contains("$player2")) { temp = temp.Replace("$player2", player2.getPlayerName()); }
+        else if (temp.Contains("$player3")) { temp = temp.Replace("$player3", player3.getPlayerName()); }
+        else if (temp.Contains("$player") && playerSave != null) { temp = temp.Replace("$player", playerSave.getPlayerName()); }
+        if (temp.Contains("$time")) { temp = temp.Replace("$time", GameInfo.info.getLastTime().ToString()); }
+        if (temp.Contains("$map")) { temp = Application.loadedLevelName; }
 
-        //$playerX
-        if(PlayerPrefs.HasKey("PlayerName1"))
-            tempText.Replace("$player1", PlayerPrefs.GetString("PlayerName1"));
-        if (PlayerPrefs.HasKey("PlayerName2"))
-            tempText.Replace("$player2", PlayerPrefs.GetString("PlayerName2"));
-        if (PlayerPrefs.HasKey("PlayerName3"))
-            tempText.Replace("$player3", PlayerPrefs.GetString("PlayerName3"));
+        if (temp.Contains("$selectedmap")) { temp = temp.Replace("$selectedmap", GameInfo.info.getSelectedMap()); }
 
-        //Apply
-        text = tempText;
+        if (temp.Contains("$selectedauthor"))
+        {
+            string aut = GameInfo.info.getSelectedAuthor();
+            if (!aut.Equals("?"))
+            {
+                temp = temp.Replace("$selectedauthor", "by " + aut);
+            }
+            else
+            {
+                temp = temp.Replace("$selectedauthor", "");
+            }
+        }
+
+        if (temp.Contains("$wr"))
+        {
+            if (wr.Equals("") && !loadingWr)
+                loadWr();
+        }
+
+        if (pb.Equals("") && playerSave != null)
+        {
+            pb = GameInfo.info.getCurrentSave().getPersonalBest(Application.loadedLevelName).ToString();
+            if (pb == "0") { pb = "-"; }
+        }
+
+        if (temp.Contains("$wr")) { temp = temp.Replace("$wr", wr); }
+        if (temp.Contains("$pb")) { temp = temp.Replace("$pb", pb); }
+
+        textScript.text = temp;
+    }
+
+    private void loadWr()
+    {
+        loadingWr = true;
+        GameInfo.info.loadMapRecord(Application.loadedLevelName, setWr);
+    }
+
+    private void setWr(string text)
+    {
+        loadingWr = false;
+        string temp = text.Split('\n')[0];
+        string[] temp2 = temp.Split('|');
+
+        if (temp2.Length == 2)
+            wr = temp2[1] + " by " + temp2[0];
     }
 }
