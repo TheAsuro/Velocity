b7816a42c8ece97e1d02df756170907084f2da9b
TheAsuro <theasuro@gmail.com>
2015-03-16 09:29:13 +0100

    Leaderboard ranks

--- Assets/Scripts/UI/LeaderboardDisplay.cs
+++ Assets/Scripts/UI/LeaderboardDisplay.cs
@@ -8,6 +8,8 @@ public class LeaderboardDisplay : MonoBehaviour
     public InputField mapNameInput;
     public List<LeaderboardPanel> entryPanels; //Must always have 10 elements!
 
+    private int startIndex = 0;
+
     void Awake()
     {
         if (mapNameInput)
@@ -16,7 +18,7 @@ public class LeaderboardDisplay : MonoBehaviour
 
     public void LoadMap(string mapName)
     {
-        StartCoroutine(Leaderboard.GetEntries(mapName, 0, DisplayData));
+        StartCoroutine(Leaderboard.GetEntries(mapName, startIndex, DisplayData));
     }
 
     private void DisplayData(string data)
@@ -25,19 +27,48 @@ public class LeaderboardDisplay : MonoBehaviour
             throw new InvalidOperationException("Data can only be displayed if entryPanels has exactly 10 elements!");
 
         string[] lines = data.Split('\n');
-        EntryRow[] rows = new EntryRow[lines.Length];
+        EntryRow[] rows = new EntryRow[10];
 
-        for(int i = 0; i < lines.Length; i++)
+        for(int i = 0; i < 10; i++)
         {
-            rows[i] = EntryRow.Parse(i + "|" + lines[i]);
+            if (i >= lines.Length || lines[i].Equals(""))
+            {
+                rows[i] = new EntryRow();
+                rows[i].isEmpty = true;
+            }
+            else
+            {
+                rows[i] = EntryRow.Parse((startIndex + i + 1) + "|" + lines[i]);
+            }
         }
 
         for(int i = 0; i < Math.Min(10, rows.Length); i++)
         {
-            entryPanels[i].time = rows[i].time.ToString("0.0000");
-            entryPanels[i].player = rows[i].player;
-            entryPanels[i].SetButtonAction(delegate { }); //TODO: Set this to download demo with demoID
+            if(rows[i].isEmpty)
+            {
+                entryPanels[i].time = "";
+                entryPanels[i].player = "";
+                entryPanels[i].rank = "";
+                entryPanels[i].SetButtonAction(delegate { });
+                entryPanels[i].SetButtonActive(false);
+            }
+            else
+            {
+                entryPanels[i].time = rows[i].time.ToString("0.0000");
+                entryPanels[i].player = rows[i].player;
+                entryPanels[i].rank = rows[i].rank;
+                entryPanels[i].SetButtonAction(delegate { }); //TODO: Set this to download demo with demoID
+                entryPanels[i].SetButtonActive(true);
+            }
         }
+    }
+
+    public void AddIndex(int add)
+    {
+        startIndex += add;
+        if (startIndex < 0)
+            startIndex = 0;
+        LoadMap(mapNameInput.text);
     }
 }
 
@@ -47,6 +78,7 @@ public struct EntryRow
     public string player;
     public decimal time;
     public int demoId;
+    public bool isEmpty;
 
     /// <summary>
     /// Creates an Entry row from a string
@@ -55,8 +87,6 @@ public struct EntryRow
     /// <returns>The parsed Entry row</returns>
     public static EntryRow Parse(string data)
     {
-        Debug.Log(data);
-
         EntryRow row = new EntryRow();
 
         if(!data.Contains("|"))
@@ -64,8 +94,8 @@ public struct EntryRow
 
         string[] parts = data.Split('|');
 
-        if(parts.Length < 3 || parts.Length > 4)
-            throw new InvalidCastException("Can't parse the string!");
+        if (parts.Length < 3 || parts.Length > 4)
+        { Debug.Log(data); throw new InvalidCastException("Can't parse the string!"); }
 
         row.rank = parts[0];
         row.player = parts[1];
@@ -76,6 +106,8 @@ public struct EntryRow
         if (parts.Length == 4)
             if(!int.TryParse(parts[3], out row.demoId))
                 throw new InvalidCastException("Can't parse the string!");
+
+        row.isEmpty = false;
 
         return row;
     }
--- Assets/Scripts/UI/LeaderboardPanel.cs
+++ Assets/Scripts/UI/LeaderboardPanel.cs
@@ -7,12 +7,14 @@ public class LeaderboardPanel : MonoBehaviour
 {
     private Text timeObj;
     private Text playerObj;
+    private Text rankObj;
     private Button donwloadButtonObj;
 
     void Awake()
     {
         timeObj = transform.FindChild("Time").GetComponent<Text>();
         playerObj = transform.FindChild("Player").GetComponent<Text>();
+        rankObj = transform.FindChild("Rank").GetComponent<Text>();
         donwloadButtonObj = transform.FindChild("GetDemo").GetComponent<Button>();
     }
 
@@ -26,9 +28,19 @@ public class LeaderboardPanel : MonoBehaviour
         get { return playerObj.text; }
         set { playerObj.text = value; }
     }
+    public string rank
+    {
+        get { return rankObj.text; }
+        set { rankObj.text = value; }
+    }
     
     public void SetButtonAction(UnityAction action)
     {
         donwloadButtonObj.onClick.AddListener(action);
+    }
+
+    public void SetButtonActive(bool value)
+    {
+        donwloadButtonObj.interactable = value;
     }
 }
