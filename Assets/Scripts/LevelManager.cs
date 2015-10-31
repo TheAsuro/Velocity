2c9592471ba35eb3031b1326c88f3290f393474e
TheAsuro <theasuro@gmail.com>
2015-10-31 03:59:32 +0100

    Updated to new unity version

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LevelManager
+{
+    private Dictionary<string, LevelInfo> levels;
+
+    //private void 
+}
+
+public struct LevelInfo
+{
+    public string displayName;
+    public string author;
+    public string path;
+}
