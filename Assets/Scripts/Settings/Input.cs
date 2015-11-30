37a0066c55da80684a088df5975b971787563fc2
TheAsuro <theasuro@gmail.com>
2015-11-30 13:46:58 +0100

    Stuff

---
+++ Assets/Scripts/Settings/Input.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+namespace Settings
+{
+    public static class Input
+    {
+        public static bool Key(KeyCode code)
+        {
+            return false; // TODO
+        }
+
+        public bool KeyDown(KeyCode code)
+        {
+            return false; // TODO
+        }
+    }
+}
