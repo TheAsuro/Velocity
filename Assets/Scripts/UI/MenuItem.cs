31099626bddc6b154b779d18ba3c60c038d52a9b
TheAsuro <theasuro@gmail.com>
2016-01-05 18:54:44 +0100

    Menu and level fixes

---
+++ Assets/Scripts/UI/MenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Selectable))]
+public class MenuItem : MonoBehaviour
+{
+    public List<KeyCode> nextKeys = new List<KeyCode>() { KeyCode.Tab };
+    public Vector3 nextDirection = Vector3.down;
+
+    void Update()
+    {
+        foreach (KeyCode nextKey in nextKeys)
+        {
+            if (Input.GetKeyDown(nextKey))
+            {
+                GetComponent<Selectable>().FindSelectable(nextDirection).Select();
+            }
+        }
+    }
+}
