608db48c0b9ec471f0e6f1095fb7d3b152a54552
Till W <theasuro@gmail.com>
2016-01-23 16:52:12 +0100

    Added new models

---
+++ Assets/Scripts/VideoPlayer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class VideoPlayer : MonoBehaviour
+{
+    private MovieTexture MTex { get { return (MovieTexture)GetComponent<Renderer>().material.mainTexture; } }
+
+    void Awake()
+    {
+        MTex.Play();
+    }
+}
