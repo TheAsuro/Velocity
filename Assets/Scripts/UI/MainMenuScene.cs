45bb37eebe3364254d7d5d350d4d02de54d365da
Till W <theasuro@gmail.com>
2017-01-19 08:59:26 +0100

    mainmenuscene

---
+++ Assets/Scripts/UI/MainMenuScene.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Demos;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class MainMenuScene : MonoBehaviour
+    {
+        [SerializeField] private string defaultDemo;
+
+        private void Awake()
+        {
+            if (defaultDemo == "")
+                throw new MissingReferenceException("Put in default demo pls");
+            else
+                LoadDemo(defaultDemo);
+        }
+
+        private void LoadDemo(string path)
+        {
+            if (File.Exists(path))
+            {
+                Demo demo = new Demo(path);
+                SceneManager.LoadScene(demo.GetLevelName(), LoadSceneMode.Additive);
+                // TODO play demo
+            }
+            else
+            {
+                throw new ArgumentException("MainMenuScene tried to laod invalid demo!");
+            }
+        }
+    }
+}
