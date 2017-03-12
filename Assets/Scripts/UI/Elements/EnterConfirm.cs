0264abc62ea0f1721e3c58e60cf74f0be4e6be8a
Till W <theasuro@gmail.com>
2017-03-12 19:53:33 +0100

    use tab/enter to navigate login menu

---
+++ Assets/Scripts/UI/Elements/EnterConfirm.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Elements
+{
+    public class EnterConfirm : MonoBehaviour
+    {
+        [SerializeField] private Button confirmButton;
+
+        private void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Return))
+                confirmButton.onClick.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Elements/TabNavigator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Elements
+{
+    [RequireComponent(typeof(Selectable))]
+    public class TabNavigator : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+                GetComponent<Selectable>().navigation.selectOnRight.Select();
+        }
+    }
+}
